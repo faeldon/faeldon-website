ded1ace626e1f0fcc1799b1538b6c50413d1b785
James Faeldon <111378+jafaeldon@users.noreply.github.com>
2019-10-18 08:56:46 +0800

    Changed headline

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import { Link, graphql } from 'gatsby'
-import styled from 'styled-components'
+import React from "react";
+import PropTypes from "prop-types";
+import { Link, graphql } from "gatsby";
+import styled from "styled-components";
 
-import { Layout, Article, Wrapper, Button, SectionTitle } from '../components'
+import { Layout, Article, Wrapper, Button, SectionTitle } from "../components";
 
 const Content = styled.div`
   grid-column: 2;
@@ -17,7 +17,7 @@ const Content = styled.div`
     padding: 2rem 1.5rem;
   }
   overflow: hidden;
-`
+`;
 
 const Hero = styled.div`
   grid-column: 2;
@@ -39,7 +39,7 @@ const Hero = styled.div`
       font-size: 1.25rem;
     }
   }
-`
+`;
 
 const IndexPage = ({
   data: {
@@ -51,12 +51,17 @@ const IndexPage = ({
       <Hero>
         <h1>Hi.</h1>
         <p>
-          I&apos;m James Faeldon a software engineer and tech entrepreneur living in Manila. And this blog is my
-          personal commitment to working more in the open.
+          I&apos;m James Faeldon a software engineer, tech entrepreneur living
+          in Manila.
         </p>
         <Link to="/contact">
           <Button big>
-            <svg width="1792" height="1792" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
+            <svg
+              width="1792"
+              height="1792"
+              viewBox="0 0 1792 1792"
+              xmlns="http://www.w3.org/2000/svg"
+            >
               <path d="M1764 11q33 24 27 64l-256 1536q-5 29-32 45-14 8-31 8-11 0-24-5l-453-185-242 295q-18 23-49 23-13 0-22-4-19-7-30.5-23.5t-11.5-36.5v-349l864-1059-1069 925-395-162q-37-14-40-55-2-40 32-59l1664-960q15-9 32-9 20 0 36 11z" />
             </svg>
             Contact
@@ -79,9 +84,9 @@ const IndexPage = ({
       </Content>
     </Wrapper>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
 
 IndexPage.propTypes = {
   data: PropTypes.shape({
@@ -89,7 +94,7 @@ IndexPage.propTypes = {
       nodes: PropTypes.array.isRequired,
     }),
   }).isRequired,
-}
+};
 
 export const IndexQuery = graphql`
   query IndexQuery {
@@ -108,4 +113,4 @@ export const IndexQuery = graphql`
       }
     }
   }
-`
+`;
